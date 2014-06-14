e0882d6a98b4ee2906b9741919b702cc448ebe8b
Micah Lee <micah@micahflee.com>
2014-06-14 04:35:11 +0000

    finished debian package, and basic install script for tails

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# note: for now this needs to be run on an internet-connected Tails computer
+
 if [[ $EUID -ne 0 ]]; then
 echo "You need to run this as root" 1>&2
     exit 1
@@ -9,4 +11,35 @@ PERSISTENT=/home/amnesia/Persistent
 INSTALL_DIR=$PERSISTENT/.securedrop-airgap
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# todo: finish install script
+VERSION=`cat version`
+
+# install dependencies
+#apt-get update
+#apt-get install -y build-essential fakeroot python-all python-stdeb
+
+# build a debian package
+rm -r deb_dist &>/dev/null 2>&1
+sudo -u amnesia python setup.py --command-packages=stdeb.command bdist_deb
+
+# setup install dir
+rm -rf $INSTALL_DIR &>/dev/null 2>&1
+mkdir -p $INSTALL_DIR
+cp $ROOT/deb_dist/securedrop-airgap_$VERSION-1_all.deb $INSTALL_DIR
+cp $ROOT/install/securedrop.png $INSTALL_DIR
+cp $ROOT/install/init.sh $INSTALL_DIR
+
+# compile the setuid hack script
+gcc -o $INSTALL_DIR/init $ROOT/install/init.c
+
+# set permissions
+chown -R root:root $INSTALL_DIR
+chmod 755 $INSTALL_DIR
+chmod 644 $INSTALL_DIR/*
+chmod 755 $INSTALL_DIR/init.sh
+chmod 755 $INSTALL_DIR/init
+chmod +s $INSTALL_DIR/init
+
+# add launcher
+cp $ROOT/install/securedrop-airgap-init.desktop /home/amnesia/Persistent
+chown amnesia:amnesia /home/amnesia/Persistent/securedrop-airgap-init.desktop
+chmod 755 /home/amnesia/Persistent/securedrop-airgap-init.desktop
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os, sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = open('version').read().strip()
+
+setup(
+    name='securedrop-airgap',
+    version=version,
+    description='',
+    long_description="""""",
+    author='Micah Lee',
+    author_email='micah@pressfreedomfoundation.org',
+    url='https://github.com/micahflee/securedrop-airgap',
+    license="GPL v2",
+    packages=['securedrop_airgap'],
+    data_files = [
+        ('/usr/share/applications', ['install/securedrop-airgap.desktop']),
+        ('/usr/share/pixmaps', ['install/securedrop.png'])
+    ],
+    scripts = ['bin/securedrop-airgap']
+)
